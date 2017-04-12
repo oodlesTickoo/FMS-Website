89c439d30e232c04f1074ea8a6460b12b3e9f1cc
himasnhusharma <himanshu.sharma@oodlestechnologies.com>
2017-04-12 17:15:02 +0530

    new structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fmsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fmsApp
+ */
+angular.module('fmsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
